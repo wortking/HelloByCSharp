77d8fe05866acfff505cdd196c697a447a967375
Alex Rodrigo Silva <AlexRodrigoSilva@hotmail.com>
2019-12-06 22:54:08 +0100

    terrtsdxkgh

---
+++ alextest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alextest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
